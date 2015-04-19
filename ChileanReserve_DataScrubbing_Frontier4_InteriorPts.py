4aa6ff3061df3d24dada810b63cabfeab71a665e
Nick Kullman <Nick.Kullman@gmail.com>
2015-04-19 14:56:47 -0700

    Add python data scrubbing file
    Created iPython nb to scrub CSV file from Java program on tothcompute
    and turn it into a file more manageable for working in Tableau to create
    visualizations.

---
+++ ChileanReserve_DataScrubbing_Frontier4_InteriorPts.py
@@ -0,0 +1,75 @@
+
+# coding: utf-8
+
+# # Restoration of Chilean eucalyptus plantation
+# ## Data wrangling exercise using Python/pandas
+
+# Goal is to take existing solution information and convert to Tableau-friendly format
+# 
+# Steps to get here:
+# 
+# 1. Model created by T. Walter, edits by N. Kullman
+# 2. Frontier corner points (ideal solutions) solved individually using standalone CPLEX runs
+# 3. Interior frontier pts found using Alpha Delta Program (calls CPLEX for optimization)
+# 4. Dominated solutions removed
+# 5. Solution files underwent one round of scrubbing through Java program built specifically for this task.
+# 
+# What's left (why we're here): Need all stands under a single column, rather than each having their own. And I want to learn data manipulation with Python/pandas.
+# 
+# Commence data manipulation...
+
+# In[329]:
+
+import pandas
+import numpy
+import pylab
+
+from pandas import *
+from pylab import *
+
+
+# In[330]:
+
+# read in and save solution data to dataframe
+reserveSolns = read_csv('ADP_20150414_192212/FrontierSolutions_SolnYearID.csv')
+
+
+# In[331]:
+
+# Strip leading spaces from column names
+reserveSolns = reserveSolns.rename(columns=lambda x: x.strip())
+
+
+# In[332]:
+
+# Stands are 'pivoted' across the table (col for each stand)
+# Here we unpivot them, creating column for stand and prescription. This will require a merge.
+# Merge's left is the original dataset, minus the stands columns
+left = reserveSolns.ix[:,:"RI"]
+# Merge's right is an unpivoted version of just the stands columns.
+# Unpivoting done with the help of the melt function
+right = pd.melt(reserveSolns,
+                id_vars = ['Solution Index', 'Year'],
+                # unpivot on the stands(first of which is XF502B)
+                value_vars = reserveSolns.columns.tolist()[reserveSolns.columns.tolist().index("XF502B"):],
+                var_name = 'Stand',
+                value_name = 'Prescription')
+# Merge to create our desired dataset
+meltedReserveSolns = merge(left, right,
+                           on = ["Solution Index", "Year"],
+                           how = 'outer')
+
+
+# In[333]:
+
+# Convert prescription column vals to int
+meltedReserveSolns["Prescription"] = meltedReserveSolns["Prescription"].apply(int)
+
+
+# In[328]:
+
+# Export to CSV for data viz with Tableau
+meltedReserveSolns.to_csv('ReserveSolutions_Frontier4_InteriorPts.csv')
+
+
+# We've finished what we set out to accomplish: learn some Python/pandas and clean a dataset for further analysis through visualization.
